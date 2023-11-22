8baab910f28995437de8846f7b010d47396c3d13
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-11-22 18:44:35 +0400

    Create Big_small.cpp

---
+++ Big_small.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+void Big_small(int **arr, int n, int m)
+{
+    int index = 0;
+    int count = 0;
+    int Big_small = arr[0][0];
+    int co = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        int max = arr[i][0];
+        for (int j = 0; j < m; ++j)
+        {
+            if (arr[i][j] > max)
+            {
+                max = arr[i][j];
+                index = j;
+            }
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            if (arr[i][index] <= max)
+            {
+                ++count;
+            }
+        }
+        if (count == 1)
+        {
+            Big_small = max;
+            ++co;
+        }
+        count = 0;
+    }
+    if (co)
+    {
+        std::cout << "The element you want is: " << Big_small;
+    }
+    else
+    {
+        std::cout << "There is no element which you want";
+    }
+}
+
+int main()
+{
+    int n = 0, m = 0;
+    std::cin >> n >> m;
+    int **arr = new int *[n];
+    for (int i = 0; i < n; ++i)
+    {
+        arr[i] = new int[m];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cin >> arr[i][j];
+        }
+    }
+    Big_small(arr, n, m);
+    std::cout << std::endl;
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+}
