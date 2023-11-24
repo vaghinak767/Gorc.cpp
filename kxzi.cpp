f92972119ef6e156b6749b8e7ae52608f5e6392b
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-11-24 20:27:15 +0400

    Update kxzi.cpp

--- kxzi.cpp
+++ kxzi.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void rek( int **arr, int n, int m)
+void rek( int **arr, int n, int m, int size, int size1)
 {
     if(n == 0 && m == 0){
         return;
@@ -12,18 +12,18 @@ void rek( int **arr, int n, int m)
             if(arr[n][m - 1] == 1)
             {
                 arr[n][m] = 0;
-                rek(arr, n , m - 1);
+                rek(arr, n , m - 1, size, size1);
                 return;
             }
             if(arr[n][m - 1] == 0)
             {
-                rek(arr, n, m - 1);
+                rek(arr, n, m - 1, size, size1);
                 return;
             }
         }
         if(arr[n][m] == 0)
         {
-            rek(arr, n, m - 1);
+            rek(arr, n, m - 1, size, size1);
             return;
         }
     }
@@ -34,19 +34,19 @@ void rek( int **arr, int n, int m)
             if(arr[n - 1][m] == 1)
             {
                 arr[n][m] = 0;
-                rek(arr, n - 1, m);
+                rek(arr, n - 1, m, size, size1);
                 return;
             }
             if(arr[n - 1][m] == 0)
             {
-                rek(arr, n - 1, m);
+                rek(arr, n - 1, m, size, size1);
                 return;
             }
         }
         if(arr[n][m] == 0)
         {
-            rek(arr, n - 1, m);
-            rek(arr, n, m - 1);
+            rek(arr, n - 1, m, size, size1);
+            rek(arr, n, m - 1, size, size1);
             return;
         }
     }
@@ -57,38 +57,55 @@ void rek( int **arr, int n, int m)
             int k = n;
                while(arr[k][m])
                {
+                    int l = m; 
+                    while(arr[k][l])
+                    {
+                        arr[k][l] = 0;
+                        if(l == 0)
+                        break;
+                        --l; 
+                    }
+                    l = m;
+                    while(arr[k][l])
+                    {
+                        arr[k][l] = 0;
+                        if(l == size1)
+                        break;
+                        ++l; 
+                    }
                     arr[k][m] = 0;
                     if(k == 0)
                     break;
                     --k;
-               } 
-               rek(arr, n - 1, m);
-               rek(arr, n, m - 1);
+               }
+               arr[n][m] = 1;
+               rek(arr, n - 1, m, size, size1);
+               rek(arr, n, m - 1, size, size1);
                return;
             }
             else if(arr[n - 1][m] == 1)
             {
                 arr[n][m] = 0;
-                rek(arr, n - 1, m);
+                rek(arr, n - 1, m, size, size1);
                 return;
             }
             else if(arr[n][m - 1] == 1)
             {
                 arr[n][m] = 0;
-                rek(arr, n, m - 1);
+                rek(arr, n, m - 1, size, size1);
                 return;
             }
             if(arr[n - 1][m] == 0 && arr[n][m - 1] == 0)
             {
-                rek(arr, n - 1, m);
-                rek(arr, n, m - 1);
+                rek(arr, n - 1, m, size, size1);
+                rek(arr, n, m - 1, size, size1);
                 return;
             }
         }
         if(arr[n][m] == 0)
         {
-            rek(arr, n - 1, m);
-            rek(arr, n, m - 1);
+            rek(arr, n - 1, m, size, size1);
+            rek(arr, n, m - 1, size, size1);
             return;
         }
     
@@ -109,7 +126,17 @@ void rek( int **arr, int n, int m)
                 std::cin >> arr[i][j];
             }
         }
-        rek(arr, n - 1, m - 1);
+        for(int i = 0; i < n; ++i)
+        {
+            for(int j = 0; j < m; ++j)
+            {
+                //sum += arr[i][j];
+                std::cout << arr[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        rek(arr, n - 1, m - 1, n, m);
         int sum = 0;
         for(int i = 0; i < n; ++i)
         {
