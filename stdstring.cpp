f99ba78f1bf83517b71e53e6365846d7911abc7f
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-11-29 18:33:35 +0400

    Update stdstring.cpp

--- stdstring.cpp
+++ stdstring.cpp
@@ -4,21 +4,54 @@ size_t strlen(const char* ptr);
 char *strchr(const char* ptr, const char c);
 int strcmp(const char* s1, const char* s2);
 char *strcpy(char *s1, const char *s2);
+char *strstr(const char *, const char *);
 
 int main()
 {
-    int size1 = 0, size2 = 0;
-    std::cin >> size1 >> size2;
-    char *s1 = new char[size1];
-    char *s2 = new char[size2];
-    std::cin >> s1;
-    std::cin >> s2;
-    std::cout << strcmp(s1, s2) << std::endl;
-    std::cout << strlen(s1) << std::endl;
-    std::cout << strchr(s1, 'k') << std::endl;
-    std::cout << strcpy(s1, s2) << std::endl;
-    delete[] s1;
-    delete[] s2;
+    int size1 = 1, size2 = 1;
+    char *arr = new char[size1];
+    char *target = new char[size2];
+    int index = 0;
+    char ch;
+    std::cin >> ch;
+    while (std::cin.get(ch) && ch != '\n') {
+        
+        if (index == size1 - 1) {
+            char* largerArray = new char[size1 * 2];
+            for (int i = 0; i < size1; ++i) {
+                largerArray[i] = arr[i];
+            }
+            delete[] arr;
+            arr = largerArray;
+            size1 *= 2;
+        }
+        arr[index] = ch;
+        ++index;
+    }
+    int index1 = 0;
+    char c;
+    while (std::cin.get(c) && c != '\n') {
+        
+        if (index1 == size2 - 1) {
+            char* largerArray = new char[size2 * 2];
+            for (int i = 0; i < size2; ++i) {
+                largerArray[i] = target[i];
+            }
+            delete[] target;
+            target = largerArray;
+            size1 *= 2;
+        }
+        target[index] = c;
+        ++index1;
+    }
+    char *ptr = strstr(arr, target);
+    std::cout << strcmp(arr, target) << std::endl;
+    std::cout << strlen(arr) << std::endl;
+    std::cout << strchr(arr, 'k') << std::endl;
+    std::cout << strcpy(arr, target) << std::endl;
+    std::cout << (void*)ptr << std::endl;
+    delete[]arr;
+    delete[]target;
 }
 
 size_t strlen(const char* ptr)
@@ -89,4 +122,34 @@ char * strcpy(char *s1, const char *s2)
     }
     *it = '\0';
     return s1;
+}
+
+char *strstr(const char *arr, const char *target)
+{
+    int size1 = 0, size2 = 0;
+    while(arr[size1])
+    {
+        ++size1;
+    }
+    while(target[size2])
+    {
+        ++size2;
+    }
+    if(size1 < size2)
+    {
+        return nullptr;
+    }
+    int tmp = 0;
+    int size = 0;
+    for(int i = 0; arr[i] != '\0'; ++i)
+    {
+        if(arr[i] != target[tmp]){
+            tmp = 0;
+        }
+        ++tmp;
+        if(target[tmp] == '\0' && (arr[i + 1] == ' ' || arr[i + 1] == '\0')){
+            return (char*)(arr + (i - tmp));
+        }
+    }
+    return nullptr;
 }
