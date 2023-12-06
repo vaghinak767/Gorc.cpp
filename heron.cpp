d3390b6750b42ece1bab2623024656e1e13677ef
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-12-06 21:33:52 +0400

    Update heron.cpp

--- heron.cpp
+++ heron.cpp
@@ -2,18 +2,18 @@
 
 double armat(double n)
 {
-    double x = 1.0;
-    for(int i = 0; i < 10; ++i)
-    {
-        x = (x + n / x) / 2;  
-    }
-    return x;
+	double x = 1.0;
+	for (int i = 0; i < 10; ++i)
+	{
+		x = (x + n / x) / 2;
+	}
+	return x;
 }
 
 bool heron(double a, double b, double c)
 {
 	double kis = (a + b + c) / 2;
-	if(a + b > c && a + c > b && b + c > a)
+	if (a + b > c && a + c > b && b + c > a)
 	{
 		std::cout << "It's a trinagle and it's surface = " << armat(kis * ((kis - a) + (kis - b) + (kis - c))) << std::endl;
 		return true;
@@ -27,7 +27,7 @@ int main()
 	std::cout << "Enter the values of tringle's sides: " << std::endl;
 	std::cin >> a >> b >> c;
 	bool pat = heron(a, b, c);
-	if(pat == false)
+	if (pat == false)
 	{
 		std::cout << "It's not a triangle " << std::endl;
 	}
