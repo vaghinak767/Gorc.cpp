943ac86a3f4042fee47b29d8bd442511f8caec02
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-12-08 21:20:49 +0400

    Update Magic_matrix.cpp

--- Magic_matrix.cpp
+++ Magic_matrix.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 
-int the_biggest(int**, int, int);
-void Big_small(int**, int, int);
+bool magic(int **arr, int n);
 
 int main()
 {
-    int n = 0, m = 0;
-    std::cout << "Enter the size of rows: ";
+    int n = 0;
+    std::cout << "Enter the sizees of matrix: ";
     std::cin >> n;
-    std::cout << "Enter the size of columns: ";
-    std::cin >> m;
     int **arr = new int *[n];
     for (int i = 0; i < n; ++i)
     {
-        arr[i] = new int[m];
+        for (int i = 0; i < n; ++i)
+        {
+            arr[i] = new int[n];
+        }
     }
-    std::cout << "Enter the elements of matrix" << std::endl;
+    std::cout << "Enter the lements of matrix" << std::endl;
     for (int i = 0; i < n; ++i)
     {
-        for (int j = 0; j < m; ++j)
+        for (int j = 0; j < n; ++j)
         {
             std::cin >> arr[i][j];
         }
     }
-    Big_small(arr, n, m);
+    std::cout << magic(arr, n) << std::endl;
     for (int i = 0; i < n; ++i)
     {
         delete[] arr[i];
@@ -31,40 +31,31 @@ int main()
     delete[] arr;
 }
 
-void Big_small(int **arr, int n, int m)
+bool magic(int **arr, int n)
 {
-    int mid = 0;
+    int sum = 0, sum_of_diagram = 0, sum_of_anti = 0;
     for (int i = 0; i < n; ++i)
     {
-        int index = the_biggest(arr, i, m);
-        for (int k = 0; k < n; ++k)
-        {
-            if (arr[k][index] < arr[i][index])
-            {
-                break;
-            }
-            ++mid;
-            if (mid == n)
-            {
-                std::cout << "The elemnt you want is arr[" << i << "][" << index << "] = " << arr[i][index] << std::endl;
-                return;
-            }
-        }
+        sum += arr[0][i];
     }
-    std::cout << "There is no element which you want" << std::endl;
-}
-
-int the_biggest(int **arr, int i, int m)
-{
-    int target = arr[i][0];
-    int index = 0;
-    for (int j = 0; j < m; ++j)
+    for (int i = 0; i < n; ++i)
     {
-        if (arr[i][j] > target)
+        int sum_of_rows = 0, sum_of_columns = 0;
+        for (int j = 0; j < n; ++j)
         {
-            target = arr[i][j];
-            index = j;
+            sum_of_rows += arr[i][j];
+            sum_of_columns += arr[j][i];
         }
+        if (sum != sum_of_columns || sum != sum_of_rows)
+        {
+            return false;
+        }
+        sum_of_diagram += arr[i][i];
+        sum_of_anti += arr[n - 1 - i][n - 1 - i];
     }
-    return index;
+    if (sum != sum_of_diagram || sum != sum_of_anti)
+    {
+        return false;
+    }
+    return true;
 }
