1c3f77b96591b59a3fb0aa263040169ab498c7da
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-12-09 15:39:22 +0400

    Create Pox_parz.cpp

---
+++ Pox_parz.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+bool stg(int, int);
+int poxparz(int);
+
+int main()
+{
+    int n = 0;
+    std::cin >> n;
+    std::cout << poxparz(n) << std::endl;
+}
+
+bool stg(int i, int n)
+{
+    for(int j = 2; j <= i; ++j)
+    {
+        if(i % j == 0 && n % j == 0)
+        return false;
+    }
+    return true;
+}
+
+int poxparz(int n)
+{
+    if(n <= 2)
+    {
+        return 0;
+    }
+    int count = 0;
+    for(int i = 2; i < n; ++i)
+    {
+        if(n % i != 0)
+        {
+            if(stg(i, n))
+            {
+                ++count;
+            };
+        }
+    }
+    return count;
+}
