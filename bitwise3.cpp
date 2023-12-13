20a894202c2ccb1fa2842297c8499dec3cae3ba7
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-12-13 17:34:58 +0400

    Update bitwise3.cpp

--- bitwise3.cpp
+++ bitwise3.cpp
@@ -13,12 +13,12 @@ void reset(int &n, int i)
 }
 void insertion(int *arr, int n)
 {
-    for(int i = 1; i < n ; ++i)
+    for (int i = 1; i < n; ++i)
     {
         int k = i;
-        for(int j = i - 1; j >= 0; --j)
+        for (int j = i - 1; j >= 0; --j)
         {
-            if(arr[j] > arr[k])
+            if (arr[j] > arr[k])
             {
                 int tmp = arr[k];
                 arr[k] = arr[j];
@@ -31,12 +31,12 @@ void insertion(int *arr, int n)
 
 void buble(int *arr, int n)
 {
-    for(int i = 0; i < n - 1; ++i)
+    for (int i = 0; i < n - 1; ++i)
     {
         bool flag = true;
-        for(int j = 0; j < n - 1 - i; ++j)
+        for (int j = 0; j < n - 1 - i; ++j)
         {
-            if(arr[j] > arr[j + 1])
+            if (arr[j] > arr[j + 1])
             {
                 int tmp = arr[j];
                 arr[j] = arr[j + 1];
@@ -44,7 +44,6 @@ void buble(int *arr, int n)
                 flag = false;
             }
         }
-        
     }
 }
 
@@ -53,20 +52,20 @@ int main()
     int n = 0;
     std::cin >> n;
     int *arr = new int[n];
-    for(int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         std::cin >> arr[i];
     }
     int key = 0;
-    while(key < 1 || key > 2)
+    while (key < 1 || key > 2)
     {
         std::cin >> key;
     }
-    if(key == 1)
-    insertion(arr, n);
-    if(key == 2)
-    buble(arr, n);
-    for(int i = 0; i < n; ++i)
+    if (key == 1)
+        insertion(arr, n);
+    if (key == 2)
+        buble(arr, n);
+    for (int i = 0; i < n; ++i)
     {
         std::cout << arr[i] << " ";
     }
