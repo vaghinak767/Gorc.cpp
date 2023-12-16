64fb01f2187fc3aff051c5099b23fcf926b9b236
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2023-12-16 16:52:35 +0400

    Update bitwise3.cpp

--- bitwise3.cpp
+++ bitwise3.cpp
@@ -39,13 +39,13 @@ void insertion(int *arr, int n)
     for (int i = 1; i < n; ++i)
     {
         int index = arr[i];
-        int j = i - 1;
-        while(j >= 0 && arr[j] > index)
+        int j = i;
+        while(j > 0 && arr[j - 1] > index)
         {
-            arr[j + 1] = arr[j];
+            arr[j] = arr[j - 1];
             --j;
         }
-        arr[j + 1] = index;
+        arr[j] = index;
     }
 }
 
